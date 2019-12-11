bf6b6c52e9a998769f24ea8282fe620986cbf50a
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-12-11 14:02:52 +0100

    Agregada la funcion de comprobacion de las reglas extra

--- src/main/java/ExpresionesRegulares/ComprobarExpresiones.java
+++ src/main/java/ExpresionesRegulares/ComprobarExpresiones.java
@@ -9,10 +9,27 @@ public class ComprobarExpresiones {
 		
 		if(expresion.indexOf("-") >= 0) {
 			if(expresion.length() == 5) {
-				for(int i = 0; i < reglas.length; i++) {
-					if(expresion.charAt(1) >= reglas[i].charAt(1) && expresion.charAt(3) <= reglas[i].charAt(3))
-						return true;
-				}
+				return comprobarReglas(expresion);
+			} else if(expresion.length() > 5 && comprobarReglas(expresion)){
+				return comprobarReglasExtra(expresion);
+			}
+		}
+		return false;
+	}
+	
+	private boolean comprobarReglas(String expresion) {
+		for(int i = 0; i < reglas.length; i++) {
+			if(expresion.charAt(1) >= reglas[i].charAt(1) && expresion.charAt(3) <= reglas[i].charAt(3))
+				return true;
+		}
+		return false;
+	}
+	
+	private boolean comprobarReglasExtra(String expresion) {
+		for(int i = 0; i < reglasExtra.length; i++) {
+			for(int j = 4; j < expresion.length(); j++) {
+				if(expresion.charAt(j) == reglasExtra[i])
+					return true;
 			}
 		}
 		return false;
