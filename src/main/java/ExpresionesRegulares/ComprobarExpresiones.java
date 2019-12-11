38868f31c05ce723f09d1ee67f3273273d2ec168
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-12-11 13:50:29 +0100

    Hecho un pequeño arreglo en la clase de comprobacion de las expresiones para permitir la introduccion de otras expresiones y la posibilidad de introducir comprobaciones de caracteres literales

--- src/main/java/ExpresionesRegulares/ComprobarExpresiones.java
+++ src/main/java/ExpresionesRegulares/ComprobarExpresiones.java
@@ -3,13 +3,16 @@ package ExpresionesRegulares;
 public class ComprobarExpresiones {
 	
 	String[] reglas = {"[a-z]","[0-9]","[A-Z]"};
+	char[] reglasExtra = {'*'};
 	
 	public boolean comprobarExpresion(String expresion) {
 		
-		if(expresion.length() == 5) {
-			for(int i = 0; i < reglas.length; i++) {
-				if(expresion.charAt(1) >= reglas[i].charAt(1) && expresion.charAt(3) <= reglas[i].charAt(3))
-					return true;
+		if(expresion.indexOf("-") >= 0) {
+			if(expresion.length() == 5) {
+				for(int i = 0; i < reglas.length; i++) {
+					if(expresion.charAt(1) >= reglas[i].charAt(1) && expresion.charAt(3) <= reglas[i].charAt(3))
+						return true;
+				}
 			}
 		}
 		return false;
