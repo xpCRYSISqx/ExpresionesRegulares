a75752577ceb5c5b96e6437e4e2c090502d6d2a4
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-12-11 14:16:56 +0100

    Empezado a escribir el metodo para comprobar mas de un caracter

--- src/main/java/ExpresionesRegulares/ComprobarEntrada.java
+++ src/main/java/ExpresionesRegulares/ComprobarEntrada.java
@@ -7,12 +7,17 @@ public class ComprobarEntrada {
 		expresion = expresion.replace("[", "");
 		expresion = expresion.replace("]", "");
 		String[] margen = expresion.split("-");
-		int inicio = margen[0].charAt(0);
-		int fin = margen[1].charAt(0);
 		
-		if(inicio <= (int) texto.charAt(0) && fin >= (int) texto.charAt(0))
-			return true;
-		else
-			return false;
+		if(expresion.indexOf("*") > 0 && texto.length() >= 1) {
+			for(int i = 0; i < texto.length(); i++) {
+				//if()
+					return true;
+			}
+		}
+		else if(texto.length() == 1) {
+			if(texto.charAt(0) >= margen[0].charAt(0) && texto.charAt(0) <= margen[1].charAt(0))
+				return true;
+		}
+		return false;
 	}
 }
